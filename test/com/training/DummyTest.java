fd58ff41678515e396019fbd847efe61f5b40615
hudson <mujir@hotmail.com>
2011-03-26 18:22:44 +0530

    adding ant script to compile and run unit tests. Also ignored the reports folder from git

---
+++ test/com/training/DummyTest.java
@@ -0,0 +1,13 @@
+package com.training;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+public class DummyTest {
+	@Test
+	public void testingJava() throws Exception {
+		Assert.assertEquals(2, 1 + 1);
+	}
+
+}
